b88832660e31a91c04457a847b2d622ba16a2f8b
Vlad <VladBashlakov@gmail.com>
2022-02-16 21:47:26 +0300

    hm 3.2. Создание API. Swagger UI, Postman

--- src/test/java/ru/hogwarts/school/ThirdCourseApplicationTests.java
+++ src/test/java/ru/hogwarts/school/ThirdCourseApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ThirdCourseApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
