40fed403bfe6b3ac01d82aae40a937e6ed3d0cea
Vlad <VladBashlakov@gmail.com>
2022-02-24 02:20:52 +0300

    3.5. Потоки данных. Работа с файлами

---
+++ src/main/java/ru/hogwarts/school/repository/AvatarRepository.java
@@ -0,0 +1,11 @@
+package ru.hogwarts.school.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.hogwarts.school.model.Avatar;
+
+
+
+public interface AvatarRepository extends JpaRepository<Avatar, Long> {
+    Avatar findAvatarById(Long id);
+
+}
