35c588f1735f08e08ecbac2351235d27db73612e
Vlad <VladBashlakov@gmail.com>
2022-03-18 21:26:10 +0300

    4.3. Миграции и индексы(правки 1)

--- src/main/resources/liquibase/scripts/student.sql
+++ src/main/resources/liquibase/scripts/student.sql
@@ -3,5 +3,11 @@
 -- changeset vbashlakov:1
 SELECT * FROM student WHERE name LIKE '12334%';
 
+
 -- changeset vvbashlakov:1
-SELECT * FROM faculty WHERE color LIKE '%string'
+SELECT * FROM faculty WHERE color LIKE '%string';
+
+-- changeset vvvbashlakov:1
+CREATE INDEX student_name_index ON student (name);
+CREATE INDEX faculty_name_and_color ON faculty (name,color);
+
