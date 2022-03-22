6d0525580178ffd2c1696339eb92a2771a76db7d
Vlad <VladBashlakov@gmail.com>
2022-03-23 01:22:56 +0300

    4.3. Миграции и индексы(рабочая версия)

--- src/main/resources/liquibase/scripts/student.sql
+++ src/main/resources/liquibase/scripts/student.sql
@@ -1,13 +1,10 @@
--- liquibase formatted sql
 
--- changeset vbashlakov:1
+
 SELECT * FROM student WHERE name LIKE '12334%';
-
-
--- changeset vvbashlakov:1
 SELECT * FROM faculty WHERE color LIKE '%string';
 
--- changeset vvvbashlakov:1
 CREATE INDEX student_name_index ON student (name);
 CREATE INDEX faculty_name_and_color ON faculty (name,color);
 
+-- DROP INDEX student_name_index;
+-- DROP INDEX faculty_name_and_color;
