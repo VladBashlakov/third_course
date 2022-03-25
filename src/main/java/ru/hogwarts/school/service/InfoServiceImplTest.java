8d679c0d3239bb71c57426dd22ed2a07d43d0d58
Vlad <VladBashlakov@gmail.com>
2022-03-25 23:45:41 +0300

    4.4 Логирование и конфигурирование в приложении(Правки 1)

--- src/main/java/ru/hogwarts/school/service/InfoServiceImplTest.java
+++ src/main/java/ru/hogwarts/school/service/InfoServiceImplTest.java
@@ -1,5 +1,6 @@
 package ru.hogwarts.school.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
@@ -7,9 +8,12 @@ import org.springframework.stereotype.Service;
 @Profile("!production")
 public class InfoServiceImplTest implements InfoService{
 
+    @Value("${serverTest.port}")
+    private String serverTestPort;
+
     @Override
     public String getPort() {
-        return "9090";
+        return serverTestPort;
     }
 
 }
